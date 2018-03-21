9b1697190aae3b04e15379def303ebe39b6125ec
fklimenka <filip.klimenka@gmail.com>
2018-03-21 19:55:42 +0000

    added IID Bootstrap for mean

--- src/Bootstrap.java
+++ src/Bootstrap.java
@@ -5,14 +5,9 @@ public class Bootstrap {
     static Random randomGenerator = new Random();
     // generate some pseudo data
     int n = 100;
-    double[] data = simulateNormal(n);
-    // compute the mean
-    double mean = getMean(data);
-    int B = 1000;               // bootstrap repetitions
-    int[] muStar = new int[B];  // place holder for bootstrapped means
-
-
-    //double theta1 = randomGenerator.nextDouble ();
+    double[] x = simulateNormal(n);
+    // bootstrap repetitions
+    int B = 1000;
 
     public double[] simulateNormal(int size)
     {
@@ -22,57 +17,59 @@ public class Bootstrap {
         return normal;
     }
 
-    public int[] simulateUniform(int size, int K)
+    public int[] simulateUniform(int size, int N)
     {
         int[] uniform = new int[size];
         for (int i = 0; i < size; i++)
-            uniform[i] = randomGenerator.nextInt(K);
+            uniform[i] = randomGenerator.nextInt(N);
         return uniform;
     }
 
-    public double getMean(double[] m)
+    public double getMean(double[] a)
     {
         double sum = 0;
-        for (int i = 0; i < m.length; i++) {
-            sum += m[i];
+        for (int i = 0; i < a.length; i++) {
+            sum += a[i];
         }
-        return sum / m.length;
+        return sum / a.length;
+    }
+
+    public double[] getIIDBootstrapSample()
+    {
+        // Uniform random numbers over 1...n
+        int u[] = simulateUniform(n, n);
+        // x-star sample simulation
+        double xStar[] = new double[n];
+        for (int i : u)
+            xStar[i] = x[i];
+        return xStar;
+    }
+
+    public double[] getIIDBootstrapMean()
+    {
+        // place holder for bootstrapped means
+        double muStar[] = new double[B];
+
+        for (int b = 0; b < B; b++)
+        {
+            double xStar[]  = getIIDBootstrapSample();
+            muStar[b] = getMean(xStar);
+        }
+        return muStar;
     }
 
     public static void main(String[] args)
     {
         Bootstrap b = new Bootstrap();
-        double[] a = b.simulateNormal(10);
-        //for (double i : a)
-        //    System.out.println(i);
-
-        int[] u = b.simulateUniform(10, 4);
-        for (int i : u)
-            System.out.println(i);
+        double muStar[] = b.getIIDBootstrapMean();
+//        for (double m : muStar)
+//            System.out.println(m);
+        System.out.println("IID bootstrap mean is " + b.getMean(muStar));
     }
 
 
 
-    /**
-     *n = 100; x = randn(n,1);
-     % Mean of x
-     mu = mean(x);
-     B = 1000;
-     % Initialize muStar
-     muStar = zeros(B,1);
-     % Loop over B bootstraps
-     for b=1:B
-     % Uniform random numbers over 1...n
-     u = ceil(n*rand(n,1));
-     % x-star sample simulation
-     xStar = x(u);
-     % Mean of x-star
-     muStar(b) = mean(xStar);
-     end
-     s2 = 1/(n-1)*sum((x-mu).^2);
-     stdErr = s2/n
-     bootstrapStdErr = mean((muStar-mu).^2)
-     */
+
 
 
 
